7e6274685a19a94598353ee85159efddc44c3bd8
JDLong <jdlong1980@gmail.com>
2018-12-05 14:47:22 -0500

    updated javascript, index and readme files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Psychic-Game
+### Instructions
+The computer has thought of a letter and you must use your incredible psychic powers to guess the letter!
+Press any letter (of the alphabet) on the keyboard.
+You have 9 guesses to either win or lose.
+After each win or loss, the computer will think of another letter!
+Enjoy!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,37 @@
-var wins = 0, losses = 0, remaining = 9;
+var wins = 0, losses = 0, remaining;
 
 var winP = document.getElementById("wins");
 var lossesP = document.getElementById("losses");
 var remainingP = document.getElementById("remaining");
 var guessesP = document.getElementById("guesses");
+var guesses;
 var compGuess;
 
 function newGame() {
-    compGuess = Math.floor(Math.random() * 26) + 65;
+    remaining = 9;
     winP.textContent = "Wins: " + wins;
     lossesP.textContent = "Losses: " + losses;
     remainingP.textContent = "Guesses left: " + remaining;
     guessesP.textContent = "Your guesses so far:";
+    guesses = [];
+    compGuess = Math.floor(Math.random() * 26) + 65;
 }
 
 document.onkeyup = function (event) {
-    if (event.keyCode > 64 && event.keyCode < 91) {
+    if (event.keyCode > 64 && event.keyCode < 91 && guesses.indexOf(event.key) === -1) {
+        console.log(guesses.indexOf(event.keycode));
+        console.log(guesses);
+        console.log(event.keyCode);
         if (event.keyCode === compGuess) {
             wins++;
             newGame();
         } else {
             remaining--;
+            guesses.push(event.key);
             remainingP.textContent = "Guesses left: " + remaining;
             guessesP.textContent += " " + event.key.toLowerCase();
             if (remaining === 0) {
                 losses++;
-                remaining = 9;
                 newGame();
             }
         }
